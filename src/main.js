5066989310e58c44ae119522fd32d71a6f3016e3
bitusr <dm.kolotov@mail.ru>
2019-05-08 14:56:17 +0200

    Implemented axes

--- src/main.js
+++ src/main.js
@@ -6,13 +6,7 @@ const HOURS_PER_DAY = 24;
 const DAYS_PER_YEAR = 365;
 const DETAILS_BARCHART_TABS = [`profit`, `revenue`, `taxes`, `employee_count`];
 
-// DATA HANDLING
-const getLastTenYearsData = (data, tab) => {
-  const years = getLastYears(Date.now(), 10).reverse();
-  const orderedData = years.map(it => data.hasOwnProperty(tab) && data[tab][`${unwrapFromQuotes(it)}`]);
-  return cleanData(orderedData);
-};
-
+// UTILS
 const getLastYears = (currentTimeInMillisecs, numberOfYears) => {
   return [...Array(numberOfYears)].map((it, i) => {
     if (i === 0) return getYear(currentTimeInMillisecs);
@@ -25,6 +19,12 @@ const getLastYears = (currentTimeInMillisecs, numberOfYears) => {
 };
 
 const getYear = currentTimeInMillisecs => new Date(currentTimeInMillisecs).getFullYear();
+
+// DATA HANDLING
+const getLastTenYearsData = (data, tab, years) => {
+  const orderedData = years.map(it => data.hasOwnProperty(tab) && data[tab][`${unwrapFromQuotes(it)}`]);
+  return cleanData(orderedData);
+};
 
 const getOneYearInMillisecs = () => MILLISECS_PER_SEC * SECS_PER_MIN * MINS_PER_HOUR * HOURS_PER_DAY * DAYS_PER_YEAR;
 
@@ -39,7 +39,8 @@ const unwrapFromQuotes = entry => {
 const cleanData = data => data.filter(it => it !== undefined);
 
 // GRAPH
-const margin = { top: 20, right: 40, bottom: 20, left: 60 };
+// =====================================================================================================================
+const margin = { top: 20, right: 40, bottom: 20, left: 50 };
 const width = 960 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
@@ -50,8 +51,12 @@ const svg = d3.select(`#wrapper`)
   .append(`g`)
   .attr(`transform`, `translate(${margin.left}, ${margin.top})`);
 
-const yScale = d3.scaleBand()
-  .range([height, 0]).padding(0.5);
+const xScale = d3.scaleBand()
+  .range([0, width])
+  .padding(0.5);
+
+const yScale = d3.scaleLinear()
+  .range([height, 0]);
 
 svg.append(`g`)
   .attr(`id`, `x-axis`)
@@ -60,12 +65,55 @@ svg.append(`g`)
 svg.append(`g`)
   .attr(`id`, `y-axis`);
 
-const update = (data, tab) => {
-  const lastTenYearsData = getLastTenYearsData(data, tab);
 
-  yScale.domain([0, d3.max(lastTenYearsData, d => +d.value)]);
+// AXIS
+const xAxis = d3.axisBottom(xScale);
 
+const yAxis = d3.axisLeft(yScale)
+  .tickSize(-width);
 
+const customXAxis = g => {
+  g.call(xAxis)
+    .select(`.domain`).remove();
+
+  g.selectAll(`.tick line`).remove()
 };
 
-update(DUMMY_DATA_NEW, `employee_count`);
+const customYAxis = g => {
+  g.call(yAxis)
+    .select(`.domain`).remove();
+
+  g.selectAll(`.tick line`)
+    .attr(`stroke`, `#d8d8d8`)
+
+  g.selectAll(`.tick text`)
+    .attr(`x`, -20)
+    .attr(`dy`, 4)
+};
+// END AXIS
+
+const update = (rawData, tab, years) => {
+  const data = getLastTenYearsData(rawData, tab, years);
+
+  xScale.domain(years);
+
+  yScale.domain([0, d3.max(data, d => +d.value)]);
+
+  let bar = svg.selectAll(`.bar`)
+    .data(data, d => d.value);
+
+  bar.exit().remove();
+
+  const barEnter = bar.enter()
+    .append(`g`)
+    .attr(`class`, `bar`)
+    .attr(`transform`, d => `translate(${xScale(+d.year)}, 0)`);
+
+  d3.select(`#x-axis`).call(customXAxis);
+
+  d3.select(`#y-axis`).call(customYAxis);
+};
+
+
+const years = getLastYears(Date.now(), 10).reverse();
+update(DUMMY_DATA_NEW, `employee_count`, years);
